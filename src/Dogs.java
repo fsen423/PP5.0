66b4f289dc11bd48678534a6c46190eea41754fc
91084579 <fsen423@gmail.com>
2019-11-14 13:47:44 -0800

    Test

---
+++ src/Dogs.java
@@ -0,0 +1,5 @@
+public class Dogs {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/dogObjects.java
@@ -0,0 +1,24 @@
+public class dogObjects {
+    private String name;
+    private int age = 0;
+    private String breed;
+    private int weight = 0;
+
+    public dogObjects(int age, String breed, int weight) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+        this.weight = weight;
+    } //end of constructor
+
+    public String toLbKg() {
+        int kg = lb * 0.453592;
+        return kg + "kg";
+    } // end of Brain Method
+
+    public String toString() {
+        String output;
+        output =
+    }
+
+}
